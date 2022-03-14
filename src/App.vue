2e3569e2ce95e8eac9790288d7938079533449e8
alexrod94 <alexrod94@gmail.com>
2022-03-14 15:32:20 +0100

    initial code

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :id="myClass">
+    <h2>Hola</h2>
+    <p>{{ counter }}</p>
+    <button v-on:click="counter++" :disabled="isDisabled">
+      Cambiar de color
+    </button>
+    <button @click="isDisabled = false">Make able</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      myClass: "card",
+      isDisabled: true,
+      counter: 1,
+    };
+  },
+  methods: {
+    changeColor() {
+      this.myClass = "card-dark";
+    },
+  },
+};
+</script>
+
+<style>
+#card {
+  background: red;
+  padding: 10px;
+  border: 1px solid orange;
+}
+
+#card-dark {
+  background: black;
+  color: white;
+  padding: 10px;
+}
+</style>
